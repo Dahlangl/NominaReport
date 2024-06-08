e9675678352fea6f3334da2504d4c1d6ab06fdd5
Sergio Gael García Landa <sergiogarcialanda046@gmail.com>
2024-06-08 14:12:49 -0600

    Vista de la pantalla de Reportes Masivos

--- NominaReport/Modulos/frmReportMasiv.Designer.cs
+++ NominaReport/Modulos/frmReportMasiv.Designer.cs
@@ -28,20 +28,229 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.pnlHeader = new System.Windows.Forms.Panel();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.pnlControls = new System.Windows.Forms.Panel();
+            this.pnlImport = new System.Windows.Forms.Panel();
+            this.pnlRevision = new System.Windows.Forms.Panel();
+            this.lblRutaArchivo = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnElegirArchivo = new System.Windows.Forms.Button();
+            this.btnProcesar = new System.Windows.Forms.Button();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.pnlHeader.SuspendLayout();
+            this.pnlControls.SuspendLayout();
+            this.pnlImport.SuspendLayout();
+            this.pnlRevision.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
             this.SuspendLayout();
+            // 
+            // pnlHeader
+            // 
+            this.pnlHeader.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlHeader.Controls.Add(this.lblTitulo);
+            this.pnlHeader.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlHeader.Location = new System.Drawing.Point(0, 0);
+            this.pnlHeader.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlHeader.Name = "pnlHeader";
+            this.pnlHeader.Size = new System.Drawing.Size(830, 66);
+            this.pnlHeader.TabIndex = 2;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(4, 4);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(275, 47);
+            this.lblTitulo.TabIndex = 0;
+            this.lblTitulo.Text = "Importar Masivo";
+            // 
+            // pnlControls
+            // 
+            this.pnlControls.Controls.Add(this.btnGuardar);
+            this.pnlControls.Controls.Add(this.btnProcesar);
+            this.pnlControls.Controls.Add(this.btnElegirArchivo);
+            this.pnlControls.Controls.Add(this.textBox2);
+            this.pnlControls.Controls.Add(this.label1);
+            this.pnlControls.Controls.Add(this.textBox1);
+            this.pnlControls.Controls.Add(this.lblRutaArchivo);
+            this.pnlControls.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlControls.Location = new System.Drawing.Point(0, 66);
+            this.pnlControls.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlControls.Name = "pnlControls";
+            this.pnlControls.Size = new System.Drawing.Size(830, 100);
+            this.pnlControls.TabIndex = 3;
+            // 
+            // pnlImport
+            // 
+            this.pnlImport.Controls.Add(this.dataGridView1);
+            this.pnlImport.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlImport.Location = new System.Drawing.Point(0, 166);
+            this.pnlImport.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlImport.Name = "pnlImport";
+            this.pnlImport.Size = new System.Drawing.Size(280, 403);
+            this.pnlImport.TabIndex = 4;
+            // 
+            // pnlRevision
+            // 
+            this.pnlRevision.Controls.Add(this.dataGridView2);
+            this.pnlRevision.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlRevision.Location = new System.Drawing.Point(280, 166);
+            this.pnlRevision.Margin = new System.Windows.Forms.Padding(0);
+            this.pnlRevision.Name = "pnlRevision";
+            this.pnlRevision.Size = new System.Drawing.Size(550, 403);
+            this.pnlRevision.TabIndex = 5;
+            // 
+            // lblRutaArchivo
+            // 
+            this.lblRutaArchivo.AutoSize = true;
+            this.lblRutaArchivo.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRutaArchivo.Location = new System.Drawing.Point(20, 19);
+            this.lblRutaArchivo.Name = "lblRutaArchivo";
+            this.lblRutaArchivo.Size = new System.Drawing.Size(86, 17);
+            this.lblRutaArchivo.TabIndex = 0;
+            this.lblRutaArchivo.Text = "Ruta Archivo";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(112, 16);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(125, 54);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(150, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(20, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(99, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N° de registros";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(28, 57);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(220, 273);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Location = new System.Drawing.Point(39, 57);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(482, 272);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // btnElegirArchivo
+            // 
+            this.btnElegirArchivo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnElegirArchivo.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnElegirArchivo.FlatAppearance.BorderSize = 0;
+            this.btnElegirArchivo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnElegirArchivo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnElegirArchivo.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnElegirArchivo.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnElegirArchivo.ForeColor = System.Drawing.Color.LightGray;
+            this.btnElegirArchivo.Location = new System.Drawing.Point(357, 27);
+            this.btnElegirArchivo.Name = "btnElegirArchivo";
+            this.btnElegirArchivo.Size = new System.Drawing.Size(129, 40);
+            this.btnElegirArchivo.TabIndex = 5;
+            this.btnElegirArchivo.Text = "Subir Archivo";
+            this.btnElegirArchivo.UseVisualStyleBackColor = false;
+            // 
+            // btnProcesar
+            // 
+            this.btnProcesar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnProcesar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProcesar.FlatAppearance.BorderSize = 0;
+            this.btnProcesar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnProcesar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnProcesar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnProcesar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnProcesar.ForeColor = System.Drawing.Color.LightGray;
+            this.btnProcesar.Location = new System.Drawing.Point(492, 27);
+            this.btnProcesar.Name = "btnProcesar";
+            this.btnProcesar.Size = new System.Drawing.Size(129, 40);
+            this.btnProcesar.TabIndex = 6;
+            this.btnProcesar.Text = "Procesar";
+            this.btnProcesar.UseVisualStyleBackColor = false;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(40)))), ((int)(((byte)(40)))), ((int)(((byte)(40)))));
+            this.btnGuardar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGuardar.FlatAppearance.BorderSize = 0;
+            this.btnGuardar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(28)))), ((int)(((byte)(28)))), ((int)(((byte)(28)))));
+            this.btnGuardar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Segoe UI", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.Color.LightGray;
+            this.btnGuardar.Location = new System.Drawing.Point(627, 27);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(129, 40);
+            this.btnGuardar.TabIndex = 7;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
             // 
             // frmReportMasiv
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(830, 569);
+            this.Controls.Add(this.pnlRevision);
+            this.Controls.Add(this.pnlImport);
+            this.Controls.Add(this.pnlControls);
+            this.Controls.Add(this.pnlHeader);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "frmReportMasiv";
             this.Text = "frmReportMasiv";
             this.Load += new System.EventHandler(this.frmReportMasiv_Load);
+            this.pnlHeader.ResumeLayout(false);
+            this.pnlHeader.PerformLayout();
+            this.pnlControls.ResumeLayout(false);
+            this.pnlControls.PerformLayout();
+            this.pnlImport.ResumeLayout(false);
+            this.pnlRevision.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel pnlHeader;
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Panel pnlControls;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblRutaArchivo;
+        private System.Windows.Forms.Panel pnlImport;
+        private System.Windows.Forms.Panel pnlRevision;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnGuardar;
+        private System.Windows.Forms.Button btnProcesar;
+        private System.Windows.Forms.Button btnElegirArchivo;
     }
 }
